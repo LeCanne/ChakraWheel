d270b64c1a01e013733053403ba39d8be9f9e6d2
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-07-01 23:03:37 +0200

    UI 320*180

---
+++ Assets/CODE/SCRIPTS/UI&RESOLUTION/CameraResizer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResizer : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+      
+
+    }
+
+
+}
