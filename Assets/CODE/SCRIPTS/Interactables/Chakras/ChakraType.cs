1c562744a3bb7f7bc5799bb99fc93f90a3c115c2
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-07-02 23:56:06 +0200

    RandomizeChakras

--- Assets/CODE/SCRIPTS/Interactables/Chakras/ChakraType.cs
+++ Assets/CODE/SCRIPTS/Interactables/Chakras/ChakraType.cs
@@ -1,27 +1,38 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ChakraType : MonoBehaviour
 {
     public Image _spriteChakra;
+    private int typeChakra;
+    public List<Sprite> spriteList;
+    private Image _image;
+    public string nameColor;
+   
     private enum CHAKRATYPE
     {
         Yellow,
         Purple,
         Blue,
-        Pink,
-        Orange,
         Red,
+        Orange,
         Green,
+        Pink,
         White
         
     }
 
+    private CHAKRATYPE chakra;
+
+
+
     private void Awake()
     {
+        _image = GetComponent<Image>();
         DefineChakra();
     }
     // Start is called before the first frame update
@@ -33,6 +44,38 @@ public class ChakraType : MonoBehaviour
     // Update is called once per frame
     void DefineChakra()
     {
+        typeChakra = Random.Range(0, spriteList.Count);
+        _image.sprite = spriteList[typeChakra];
 
+        switch (typeChakra)
+        {
+            case 0:
+                chakra = CHAKRATYPE.Yellow;
+                break;
+            case 1:
+                chakra = CHAKRATYPE.Purple;
+                break;
+            case 3:
+                chakra = CHAKRATYPE.Blue;
+                break;
+            case 4:
+                chakra = CHAKRATYPE.Red;
+                break;
+            case 5:
+                chakra = CHAKRATYPE.Orange;
+                break;
+            case 6:
+                chakra = CHAKRATYPE.Green;
+                break;
+            case 7:
+                chakra = CHAKRATYPE.Pink;
+                break;
+            case 8:
+                chakra = CHAKRATYPE.White;
+                break;
+
+
+        }
+       
     }
 }
